625107fce9894e1c0c762ab90eddbadb86c335d9
Itz-SuleX5 <itzsulex5@gmail.com>
2025-06-27 08:00:14 -0700

    feat: Dynamic fetch from supabase to 'cardSection.tsx'.

--- src/components/organisms/admin.tsx
+++ src/components/organisms/admin.tsx
@@ -39,8 +39,49 @@ const Admin = () => {
                             </div>
                             <PrincipalDnD />
                         </div>
+
+                        <div className="w-full bg-stone-50  flex flex-col gap-3 p-4 mt-4 rounded-xl border-2 border-dotted border-purple-300">
+                            <div className="flex items-center gap-1 text-blue-600">
+                                <DynamicIcon name="tag" color="currentColor" className="w-4 h-4" />
+                                <h1 className="text-l font-medium">Detalles del producto</h1>
+                            </div>
+                            <div>
+                                <h1>Titulo</h1>
+                                <input type="text" className="border border-purple-300 rounded w-full py-1 indent-2" placeholder="Nombre del producto"/>
+                            </div>
+                            <div className="flex flex-col grid grid-cols-2 ">
+                                <div>
+                                    <h1>Precio</h1>
+                                <div className="relative flex items-center">
+                                    <div className="absolute left-2">
+                                        <h1 className="text-gray-500">$</h1>
+                                    </div>
+                                    
+                                    <input type="text" className="border border-purple-300 rounded py-1 indent-6 w-full" placeholder="0.0"/>
+                                </div>
+                                </div>
+                                <div>
+                                    <h1>Categoria</h1>
+                                    <select name="" id="" >
+                                        
+
+
+                                    </select>
+                                </div>
+                                
+                                
+                            </div>
+                            
+                        </div>
+
                     </div>
+
+
+                    
                 </div>
+
+                
+
             </div>
         </div>
     );
--- src/components/organisms/cardSection.tsx
+++ src/components/organisms/cardSection.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../molecules/card";
 
 
@@ -41,22 +41,41 @@ const testProduct = [{
 
 
 ];
-const CardSection = () => (
+const CardSection = () => {
+    const [products, setProducts] = useState([])
+    const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+    const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+    useEffect(() => {
+  fetch(`${supabaseUrl}/rest/v1/products?select=*,categories(name)`, {
+    headers: {
+      apikey: supabaseKey,
+      Authorization: `Bearer ${supabaseKey}`,
+    },
+  })
+    .then(res => res.json())
+    .then(data => setProducts(data))
+    .catch(err => console.error(err));
+}, [supabaseUrl, supabaseKey]);
+
+  return (
     <div className="bg-yellow-50 flex w-full p-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         
-        {testProduct.map((testProduct) => (
-        <Card 
+        {products.map((testProduct) => (
+            <Card 
+                key={testProduct.id}
                 title={testProduct.title}
                 price={testProduct.price}
-                imageUrl={testProduct.imageUrl}
-                category={testProduct.category}
-                />
+                imageUrl={testProduct.main_image_url}
+                category={testProduct.categories?.name}
+            />
          ))}
             
         </div>
         
     </div>
 );
+};
 
 export default CardSection;
